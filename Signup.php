02c92dde66adf8a9147c6960a8a95a39818b0db4
Andrew Dong <andrew.mh.dong@gmail.com>
2016-03-25 15:19:24 -0600

    login page now works. You can test with andrew@gmail.com and PASSWORD = 1234

--- Signup.php
+++ Signup.php
@@ -1,7 +1,4 @@
-<?php
-    session_start();
-
-?><!DOCTYPE html>
+<!DOCTYPE html>
 
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -16,7 +13,7 @@
     <nav class="navbar navbar-inverse navbar-fixed-top">
         <div class="container-fluid">
             <div class="navbar-header">
-                <a class="navbar-brand" href="Home.html">
+                <a class="navbar-brand" href="#">
                     <img alt="Brand" class="brand" src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Boostrap_logo.svg/2000px-Boostrap_logo.svg.png">
                 </a>
                 <p class="navbar-text" style="color: white; font-weight: bold; font-size: 18px;">
@@ -28,7 +25,7 @@
             </div>
             <div class="collapse navbar-collapse" id="Navbar">
                 <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#">My Bio</a></li>
+                    <li><a href="#">Submit</a></li>
                     <li class="dropdown">
                         <a class="dropdown-toggle" data-toggle="dropdown" href="#">Categories  <span class="glyphicon glyphicon-menu-down"><small></small></span></a>
                         <ul class="dropdown-menu">
@@ -54,27 +51,55 @@
             <p id="legal-text">@Legal Stuff goes here with social media links</p>
         </div>
     </nav>
-       
-    <div class="row">
-        <form class = "form-signin" name = "form1" method = "post" action = "loginVerify.php">
-        <div class="col-xs-12 col-sm-6 col-md-6">
-                <h1 style="margin-bottom: 20px">Login</h1>
-                <p id="form-label">Gurl whats yo Emaail:</p>
-                <input name="myusername" id="myusername" type="text" class="form-control" placeholder="Email Address" autofocus>
-                
-                <p id="form-label">DANKEST PASSWORD</p>
-                <input name="mypassword" id="mypassword" type="password" class="form-control" placeholder="Password">
-                <button type="login" class="btn" id="form-submit">Login</button>
-        </div>
 
-        <div class="col-xs-12 col-sm-6 col-md-6">
-            <h1 style="margin=bottom: 20px">Sign Up</h1>
-            <p id="signup-text">Join our website today to comment on art pieces, talk to artists and post your own work!</p>
-            <p>Also, you can recieve feedback from other people around the world, along with ratings, critiques, and much more.</p>
-            <button onclick="location.href='Signup.html'" type="button" class="btn" id="form-signup" href="Signup.html">Sign Up</button>
+    <h1 style="margin-bottom: 20px;">Create an Account</h1>
+
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-6">
+            <p id="form-label">First Name</p>
+            <input type="text" class="form-control">
         </div>
-        </form>
-    </div>
-    
+        <div class="col-xs-12 col-sm-12 col-md-6">
+            <p id="form-label">Last Name</p>
+            <input type="text" class="form-control" id="basic-url">
+        </div>
+        <div class="col-xs-12">
+            <p id="form-label">UserName</p>
+            <input type="text" class="form-control" id="basic-url" />
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-6">
+            <p id="form-label">Password:</p>
+            <input type="password" class="form-control">
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-6">
+            <p id="form-label">Re-enter your pusheen password</p>
+            <input type="password" class="form-control" id="basic-url">
+        </div>
+        <div class="col-xs-12">
+            <p id="form-label">Email</p>
+            <input type="text" class="form-control" id="basic-url" />
+        </div>
+        <div class="col-xs-12">
+            <p id="form-label">Re-Enter Email</p>
+            <input type="text" class="form-control" id="basic-url" />
+        </div>
+        <div class="col-xs-12">
+            <p id="form-label">Country</p>
+            <select name="Country" id="form-dropdown">
+                <option>Canada</option>
+                <option>United States</option>
+                <option>Europe</option>
+                <option>Australia</option>
+            </select>
+        </div>
+        <div class="col-xs-12" id="privacy-box">
+            <form>
+                <input type="checkbox"> I agree to this <a href="#">Privacy Agreement</a><br>
+            </form>
+        </div>
+    </div>   
+
+    <button type="button" class="btn" id="form-submit">follow</button>
+
 </body>
 </html>
--- loginVerify.php
+++ loginVerify.php
@@ -1,5 +1,5 @@
 <?php
-
+    session_start();
     $link = mysqli_connect("localhost", "root", "", "seng513_perspectiv");
 
 	if($link === false)
@@ -10,4 +10,23 @@
         echo nl2br ("NICE CONNECTED!\n");
     }
     
+    $email_address = mysqli_real_escape_string($link,$_POST['myusername']);
+	$user_password = mysqli_real_escape_string($link,$_POST['mypassword']);
+    
+
+    
+    $sql = "select * from user where password='$user_password' AND email='$email_address'";
+    $result = mysqli_query($link,$sql);
+    $rows = mysqli_num_rows($result);
+    if ($rows == 1) 
+    {
+        echo nl2br ("Welcome to the club \n");
+        $_SESSION['login_user']=$email_address; // Initializing Session
+        header("refresh:5; home.html");
+
+    } 
+    else 
+    {
+         echo nl2br ("OH MAN WRONG EMAIL OR PASSWORD\n");
+    }
 ?>
